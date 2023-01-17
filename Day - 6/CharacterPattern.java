837621a651432a5147a806f36fd709ef409128bb
Shiv Singh <singhshiv0204@gmail.com>
2023-01-18 02:19:25 +0530

    Completed Day - 6

---
+++ Day - 6/CharacterPattern.java
@@ -0,0 +1,14 @@
+
+public class CharacterPattern {
+    public static void main(String[] args) {
+        
+        char ch = 'A';
+        for(int line=1; line<=4; line++){
+            for(int character=1; character<=line; character++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
